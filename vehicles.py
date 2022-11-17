95da9ddd2945c86eac642c6f86c4f1923b53f573
alina <104669971+caratsl@users.noreply.github.com>
2022-11-18 00:28:20 +0700

    new field

--- vehicles.py
+++ vehicles.py
@@ -22,18 +22,20 @@ class Vehicle:
         self.power = 0
         self.tmp_char = 0
         self.id = 0
+        self.consumption = 0
 
     def get_id(self):
         return self.id
 
-    def set_char(self, key, power, tmp_char, idd):
+    def set_char(self, key, power, tmp_char, idd, con):
         self.key = key
         self.power = power
         self.tmp_char = tmp_char
         self.id = idd
+        self.consumption = con
 
     def get_char(self):
-        return self.id, self.power, self.tmp_char
+        return self.id, self.power, self.tmp_char, self.consumption
 
 
 f = open('in.txt')
@@ -45,8 +47,9 @@ def in_Bus(new_veh):
     key = 'BUS'
     power = int(a[0])
     tmp = int(a[1])
+    con = int(a[2])
     idd = gen_id(objects)
-    new_veh.set_char( key, power, tmp, idd)
+    new_veh.set_char( key, power, tmp, idd, con)
 
     return new_veh
 
@@ -57,20 +60,21 @@ def in_Truck(new_veh):
     key = 'TRUCK'
     power = int(a[0])
     tmp = int(a[1])
+    con = int(a[2])
     idd = gen_id(objects)
-    new_veh.set_char( key, power, tmp, idd)
+    new_veh.set_char( key, power, tmp, idd, con)
 
     return new_veh
 
 
 def out_Bus(out_veh):
     a = out_veh.get_char()
-    print(f'{a[0]}: It is BUS, Power = {a[1]}, capacity = {a[2]}')
+    print(f'{a[0]}: It is BUS, Power = {a[1]}, capacity = {a[2]}, consumption = {a[3]}')
 
 
 def out_Truck(out_veh):
     a = out_veh.get_char()
-    print(f'{a[0]}: It is TRUCK, Power = {a[1]}, maxWeight = {a[2]}')
+    print(f'{a[0]}: It is TRUCK, Power = {a[1]}, maxWeight = {a[2]}, consumption = {a[3]}')
 
 
 def clear_container():
