42835f759bb73acd1ad887c5cc45a9bfd910740f
caratsl <104669971+caratsl@users.noreply.github.com>
2022-11-17 22:36:00 +0700

    1 lab

---
+++ main.py
@@ -0,0 +1,19 @@
+from vehicles import *
+
+
+
+file_in()
+if len(objects) == 0:
+    print("Container is empty")
+else:
+    print("Container is filled")
+    print(f"Contains {len(objects)} element(s)")
+    for i in objects:
+        if i.key == 'BUS':
+            out_Bus(i)
+        elif i.key == 'TRUCK':
+            out_Truck(i)
+    clear_container()
+    print("Container is empty")
+    print(f"Contains {len(objects)} element(s)")
+
---
+++ vehicles.py
@@ -0,0 +1,95 @@
+objects = []
+
+
+def gen_id(arr):
+    ids = 0
+    check = True
+    while check:
+        check_tmp = False
+        for i in arr:
+            if i.get_id() == ids:
+                check_tmp = True
+        if check_tmp:
+            ids = ids + 1
+        else:
+            check = False
+    return ids
+
+
+class Vehicle:
+    def __init__(self):
+        self.key = ''
+        self.power = 0
+        self.tmp_char = 0
+        self.id = 0
+
+    def get_id(self):
+        return self.id
+
+    def set_char(self, key, power, tmp_char, idd):
+        self.key = key
+        self.power = power
+        self.tmp_char = tmp_char
+        self.id = idd
+
+    def get_char(self):
+        return self.id, self.power, self.tmp_char
+
+
+f = open('in.txt')
+
+
+def in_Bus(new_veh):
+    text = f.readline()
+    a = text.split(' ')
+    key = 'BUS'
+    power = int(a[0])
+    tmp = int(a[1])
+    idd = gen_id(objects)
+    new_veh.set_char( key, power, tmp, idd)
+
+    return new_veh
+
+
+def in_Truck(new_veh):
+    text = f.readline()
+    a = text.split(' ')
+    key = 'TRUCK'
+    power = int(a[0])
+    tmp = int(a[1])
+    idd = gen_id(objects)
+    new_veh.set_char( key, power, tmp, idd)
+
+    return new_veh
+
+
+def out_Bus(out_veh):
+    a = out_veh.get_char()
+    print(f'{a[0]}: It is BUS, Power = {a[1]}, capacity = {a[2]}')
+
+
+def out_Truck(out_veh):
+    a = out_veh.get_char()
+    print(f'{a[0]}: It is TRUCK, Power = {a[1]}, maxWeight = {a[2]}')
+
+
+def clear_container():
+    objects.clear()
+
+def file_in():
+    check = True
+    while check:
+        text = f.readline()
+        if text == '':
+            check = False
+        else:
+            text = text.split('\n')
+            type = int(text[0])
+            if type == 1:  # это BUS
+                veh = Vehicle()
+                veh = in_Bus(veh)
+                objects.append(veh)
+            elif type == 2:
+                veh1 = Vehicle()
+                veh1 = in_Truck(veh1)
+                objects.append(veh1)
